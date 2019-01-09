9498a4342824f2fe84b42562c6cf7e34181ac5e6
mariagbl <maria_barsallo_lynch@student.hks.harvard.edu>
2019-01-08 21:28:40 -0500

    java script calculator code

---
+++ calculator.js
@@ -0,0 +1,23 @@
+function multiply(a,b) { 
+    return a * b
+};
+var x = multiply(8,9)
+
+function add(a,b) { 
+    return a + b
+};
+var x = add(8,9)
+
+function divide(a,b) { 
+    return a / b
+};
+var x = divide(8,9)
+
+function subtract(a,b) { 
+    return a - b
+};
+var x = subtract(8,9)
+
+
+
+
